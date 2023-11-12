38728b69559d98b438da55020f0b5d0ab9d59880
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-11-11 21:15:06 -0300

    Starting navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import styled from "styled-components"
+import { TopMenu } from "./components/TopMenu"
+
+const Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: #FFFFFF;
+`
+
 export function App() {
 
   return (
-    <div>
-
-    </div>
+    <Main>
+      <TopMenu/>
+    </Main>
   )
 }
---
+++ src/components/TopMenu.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  position: fixed;
+  display: flex;
+  align-items:center;
+  justify-content: space-between;
+  padding-left: 70px;
+  padding-right: 70px;
+  background: #0F52BA;
+  width: 100%;
+  height: 101px;
+`
+const ContentLogo = styled.div`
+  height: 50px;
+  display: flex;
+  gap: 8px;
+  padding: 2px;
+  align-items: center;
+  flex-direction: row;
+`
+const Logo = styled.h1`
+  color: #FFFFFF;
+  line-height: 19px;
+  font-size: 40px;
+  font-weight: 600;
+`
+const Subtitle = styled.h3`
+  margin-top: 12px;
+  color: #FFFFFF;
+  line-height: 19px;
+  font-size: 20px;
+  font-weight: 300;
+`
+
+export function TopMenu() {
+
+  return (
+    <Nav>
+      <ContentLogo>
+        <Logo>MKS</Logo>
+        <Subtitle>Sistemas</Subtitle>
+      </ContentLogo>
+    </Nav>
+  )
+}
