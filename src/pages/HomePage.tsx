689b35e4fb3f7b89dd67bb2de961e3f7f4755432
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-11-12 12:59:12 -0300

    Fix: Name conflict error on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,22 @@
 import styled from "styled-components"
 import { Navbar } from "../components/Navbar"
-import { Content } from "../components/Main"
+import { Main } from "../components/Main"
 import { Footer } from "../components/Footer"
 
-const Main = styled.div`
+export function HomePage() {
+  return(
+    <Wrapper>
+      <Navbar/>
+      <Main/>
+      <Footer/>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
   background: #FFFFFF;
   display: flex;
   flex-direction: column;
 `
-
-export function HomePage() {
-  return(
-    <Main>
-      <Navbar/>
-      <Content/>
-      <Footer/>
-    </Main>
-  )
-}
